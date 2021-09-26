34fe7c4aca765e6951fb8f4ee81f8d120354c29e
ceaser501 <ceaser501@naver.com>
2021-09-26 23:59:38 +0900

    웹 계층 개발 - 회원등록

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -25,7 +25,10 @@ public class MemberController {
     }
 
     @PostMapping("/members/new")
-    public String create(@Valid MemberForm form, BindingResult result){         // @Valid 쓰면 javax.validation 쓰겠다는 뜻! MemberForm.java에 @NotNull 같은 것들..
+    public String create(@Valid MemberForm form, BindingResult result){
+        // @Valid 쓰면 javax.validation 쓰겠다는 뜻! MemberForm.java에 @NotNull 같은 것들..
+        // Member Entity를 쓰지 않고, form을 새로 생성해서 new로 model에 담아 넘기는 이유는, 꼭 필요한 것만 가져다 쓰기 위함이다.
+        // Member를 그대로 쓴다면, 불필요한 변수들도 있고 validation을 MemberEntity에 작성하면 소스가 지저분해 질 수 있기 때문이다.
         Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
 
         if(result.hasErrors()){

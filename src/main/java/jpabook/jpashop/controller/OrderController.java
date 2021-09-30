833c6c25a0dce7e89be886812819fd3f5caefbd1
ceaser501 <ceaser501@naver.com>
2021-10-01 02:17:49 +0900

    웹 계층 개발 [상품 수정, 상품 주문, 주문목록 검색/취소]

---
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model){
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,        // form summit name 으로 넘긴 걸 받아오기 위해 쓰는 어노테이션
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+
+        orderService.order(memberId, itemId, count);
+
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model){
+
+        List<Order> orders = orderService.findOrders(orderSearch);
+        // 단순 위임이므로 바로 repository를 불러 findOrders를 구현해도 된다
+
+        model.addAttribute("orders", orders);
+        // @ModelAttribute 가 있으면 아래 구문이 추가되어 자동으로 화면으로 내려간다고 보면 된다
+        // model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
+}
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,17 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);
+
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+//        itemRepository.save(findItem); 할 필요가 없음
+//        transaction이 commit이 되면 JPA가 flush를 날리고, 그러면 엔티티의 변경된 부분을 찾아 DB를 업데이트 친다
     }
 
     public List<Item> findItems(){
